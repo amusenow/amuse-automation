199da341fc7b9d430ad6d852f19b9c52bb5ede65
Denys Fedotov <denysfedotov@Denyss-MacBook-Pro.local>
2021-11-09 19:55:25 +0200

    wip: demo prep

--- config/iosBrowser.config.js
+++ config/iosBrowser.config.js
@@ -6,6 +6,7 @@ const config = require('./wdio.conf.js').config
 // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
 config.capabilities = [{
   //The defaults you need to have in your config
+  "browserstack.useW3C": "false",
   'browserstack:device': 'iPhone 11',
   'browserstack:os_version': '13',
   'browserName': 'iPhone',
@@ -15,7 +16,7 @@ config.capabilities = [{
   "browserstack.debug": "true",
   "browserstack.console": "error",
   "browserstack.networkLogs" : "true",
-  "browserstack.networkLogsOptions": {
+  "browserstack:networkLogsOptions": {
     "captureContent": "true"
   },
   // browserName: 'safari',
