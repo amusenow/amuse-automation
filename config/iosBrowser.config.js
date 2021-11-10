325c0c68c1abace906bf1c3cad7f32985b2e8ef7
Denys Fedotov <denysfedotov@Denyss-MacBook-Pro.local>
2021-11-10 03:29:33 +0200

    wip: demo prep

--- config/iosBrowser.config.js
+++ config/iosBrowser.config.js
@@ -5,14 +5,15 @@ const config = require('./wdio.conf.js').config
 // For all capabilities please check
 // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
 config.capabilities = [{
-  "os_version" : "13",
-  "device" : "iPhone 11",
-  "real_mobile" : "true",
-  "browserstack.appium_version" : "1.22.0",
-  "browserstack.local" : "false",
-  "browserstack.debug" : "true",
-  "browserstack.networkLogs" : "true",
-  "browserName" : "iPhone"
+  'bstack:options' : {
+    "osVersion" : "14",
+    "deviceName" : "iPhone 12",
+    "realMobile" : "true",
+    "appiumVersion" : "1.22.0",
+    "local" : "false",
+    "debug" : "true"
+  },
+  "browserName" : "iPhone",
 }];
 config.cucumberOpts.tagExpression = '@iosBrowser'; // pass tag to run tests specific to ios
 
